f966dcafc13e5d6abd58f26f118a7915d16f2b98
ANISH M <58029804+Anish-M-code@users.noreply.github.com>
2020-03-15 22:56:59 +0530

    Add files via upload

---
+++ notepad++_hack.c
@@ -0,0 +1,20 @@
+
+/*
+   This C Program was developed by M.Anish to demonstrate the importance of Digital Signatures
+   and show that Programs can be modified even without the source code. It is not particularly hard though
+   to modify programs for malicious purpose.
+
+   For more details visit: https://outflaw.blogspot.com/2019/11/way-to-modify-c-and-c-programs-without.html
+*/
+
+ //This program is for windows. Intended to be compiled with mingw c compiler only.
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main()
+{
+    printf("\nThis Program was developed by: M.Anish\n");
+    system("start https://kali.org");
+    system("pause");
+}
