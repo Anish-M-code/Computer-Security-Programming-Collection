3d726009f63b9e77e102497402d7ba326a2a654d
ANISH M <58029804+Anish-M-code@users.noreply.github.com>
2020-08-17 22:58:57 +0530

    Update insecure_login.py

--- insecure_login.py
+++ insecure_login.py
@@ -1,9 +1,9 @@
 
 #This Program was developed by M.Anish to show flaws of eval() in python3.
-
+#Giving __import__('os').system("<malicious command> ") can damage such a system.
 import getpass
 
-passwd='a'
+passwd=1234
 user=input('Enter username:')
 password=getpass.getpass('\nEnter PIN:')
 password=eval(password)
